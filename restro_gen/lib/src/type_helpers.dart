f87a5531a073dd91cbf14624fc8f9c0ca1c64562
Ganesh Poojary <ganesh4212@gmail.com>
2019-01-08 20:35:13 +0530

    removed unused code.

--- restro_gen/lib/src/type_helpers.dart
+++ restro_gen/lib/src/type_helpers.dart
@@ -1,10 +1,5 @@
-import 'dart:async';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/type.dart';
-import 'package:build/src/builder/build_step.dart';
-import 'package:source_gen/source_gen.dart';
 import 'package:code_builder/code_builder.dart';
-import 'package:restro/restro.dart';
 import 'annotation_helpers.dart';
 import 'restro_gen_config.dart';
 import 'utils.dart';
@@ -53,21 +48,5 @@ abstract class TypeHelpers {
     AnnotationHelpers.processMethodParamPathAnnotation(
         config, parameterElement);
     return parameterBuilder.build();
-  }
-
-  static void processMethodAnnotation(ElementAnnotation elementAnnotation) {
-    print(
-        "elementAnnotation.constantValues : ${elementAnnotation.constantValue.getField('value').toStringValue()} *****");
-    print(
-        "elementAnnotation.runtimeType : ${elementAnnotation.constantValue.type.name} *****");
-    print(
-        "elementAnnotation.runtimeType : ${elementAnnotation.runtimeType} *****");
-    print(
-        "elementAnnotation.source.fullName : ${elementAnnotation.source.fullName} *****");
-    print(
-        "elementAnnotation.element.name : ${elementAnnotation.element.name} *****");
-    print(
-        "elementAnnotation.element.displayName : ${elementAnnotation.element.displayName} *****");
-    final getTypeChecker = TypeChecker.fromRuntime(GET);
   }
 }
